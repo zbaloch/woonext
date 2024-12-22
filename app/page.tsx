acd61d002647489302a33d2d76511e0ccd65dde0
zaheerbaloch <zaheer@hey.com>
2024-12-22 09:06:28 +0300

    Changes - WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,247 @@
-import Image from "next/image";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navigation from "@/components/Navigation";
+import { gql, request } from 'graphql-request';
 
-export default function Home() {
+const products = [
+  {
+    id: 1,
+    name: 'Machined Pen',
+    color: 'Black',
+    price: '$35',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-02-product-01.jpg',
+    imageAlt: 'Black machined steel pen with hexagonal grip and small white logo at top.',
+    availableColors: [
+      { name: 'Black', colorBg: '#111827' },
+      { name: 'Brass', colorBg: '#FDE68A' },
+      { name: 'Chrome', colorBg: '#E5E7EB' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Machined Pen',
+    color: 'Black',
+    price: '$35',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-02-product-01.jpg',
+    imageAlt: 'Black machined steel pen with hexagonal grip and small white logo at top.',
+    availableColors: [
+      { name: 'Black', colorBg: '#111827' },
+      { name: 'Brass', colorBg: '#FDE68A' },
+      { name: 'Chrome', colorBg: '#E5E7EB' },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Machined Pen',
+    color: 'Black',
+    price: '$35',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-02-product-01.jpg',
+    imageAlt: 'Black machined steel pen with hexagonal grip and small white logo at top.',
+    availableColors: [
+      { name: 'Black', colorBg: '#111827' },
+      { name: 'Brass', colorBg: '#FDE68A' },
+      { name: 'Chrome', colorBg: '#E5E7EB' },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Machined Pen',
+    color: 'Black',
+    price: '$35',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-02-product-01.jpg',
+    imageAlt: 'Black machined steel pen with hexagonal grip and small white logo at top.',
+    availableColors: [
+      { name: 'Black', colorBg: '#111827' },
+      { name: 'Brass', colorBg: '#FDE68A' },
+      { name: 'Chrome', colorBg: '#E5E7EB' },
+    ],
+  },
+]
+
+const categories = [
+  {
+    name: 'New Arrivals',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-01-category-01.jpg',
+  },
+  {
+    name: 'Productivity',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-01-category-02.jpg',
+  },
+  {
+    name: 'Workspace',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-01-category-04.jpg',
+  },
+  {
+    name: 'Accessories',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-01-category-05.jpg',
+  },
+  {
+    name: 'Sale',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/home-page-01-category-03.jpg',
+  },
+]
+
+export default async function Home() {
+
+  const endpoint = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+  
+  const productsCategoriesQuery = gql`
+    query {
+      products (first: 4, where: {orderby: {field: DATE, order: DESC}}) {
+        nodes {
+          slug
+          productid: databaseId
+          name
+          onSale
+          sku
+          shortDescription
+          type
+          featured
+          image {
+            altText
+            id
+            sourceUrl
+            srcSet
+          }
+          ... on SimpleProduct {
+            price
+            regularPrice
+            id
+          }
+          ... on VariableProduct {
+            price
+            id
+            regularPrice
+          }
+          ... on ExternalProduct {
+            price
+            id
+            externalUrl
+            regularPrice
+          }
+        }
+      }
+      productCategories (first: 5) {
+        nodes {
+          id
+          name
+          slug
+          image {
+            altText
+            id
+            sourceUrl
+            srcSet
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await request(endpoint, productsCategoriesQuery) as { products: { nodes: any[] }, productCategories: { nodes: any[] } };
+  const products = data.products.nodes;
+  const categories = data.productCategories.nodes;
+  console.log(products);
+  console.log(categories);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   <>
+      <Navigation /> 
+      <main>
+        <Hero />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="bg-white">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <div className="md:flex md:items-center md:justify-between">
+              <h2 className="text-2xl font-bold tracking-tight text-gray-900">New products</h2>
+              <a href="/products" className="hidden text-sm font-medium text-indigo-600 hover:text-indigo-500 md:block">
+                Shop all products
+                <span aria-hidden="true"> &rarr;</span>
+              </a>
+            </div>
+
+            <div className="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
+              {products.map((product) => (
+                <div key={product.id} className="group relative">
+                  <div className="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80">
+                    <img alt={product.image.altText} src={product.image.sourceUrl} srcSet={product.image.srcSet} className="size-full object-cover" />
+                  </div>
+                  <h3 className="mt-4 text-sm text-gray-700">
+                    <a href={'/products/' + product.slug}>
+                      <span className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  {/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
+                  <p className="mt-1 text-sm font-medium text-gray-900">{product.price}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className="mt-8 text-sm md:hidden">
+              <a href="/products" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Shop all products
+                <span aria-hidden="true"> &rarr;</span>
+              </a>
+            </div>
+          </div>
         </div>
+
+        {/* <div className="bg-white">
+          <div className="py-16 sm:py-24 xl:mx-auto xl:max-w-7xl xl:px-8">
+            <div className="px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8 xl:px-0">
+              <h2 className="text-2xl font-bold tracking-tight text-gray-900">Shop by Category</h2>
+              <a href="#" className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block">
+                Shop all products
+                <span aria-hidden="true"> &rarr;</span>
+              </a>
+            </div>
+
+            <div className="mt-4 flow-root">
+              <div className="-my-2">
+                <div className="relative box-content h-80 overflow-x-auto py-2 xl:overflow-visible">
+                  <div className="absolute flex space-x-8 px-4 sm:px-6 lg:px-8 xl:relative xl:grid xl:grid-cols-5 xl:gap-x-8 xl:space-x-0 xl:px-0">
+                    {categories.map((category) => (
+                      <a
+                        key={category.name}
+                        href={category.href}
+                        className="relative flex h-80 w-56 flex-col overflow-hidden rounded-lg p-6 hover:opacity-75 xl:w-auto"
+                      >
+                        <span aria-hidden="true" className="absolute inset-0">
+                          <img alt="" src={category.imageSrc} className="size-full object-cover" />
+                        </span>
+                        <span
+                          aria-hidden="true"
+                          className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"
+                        />
+                        <span className="relative mt-auto text-center text-xl font-bold text-white">{category.name}</span>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6 px-4 sm:hidden">
+              <a href="#" className="block text-sm font-semibold text-indigo-600 hover:text-indigo-500">
+                Shop all products
+                <span aria-hidden="true"> &rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div> */}
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+      <Footer />
+   </>
   );
 }
---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,356 @@
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navigation from "@/components/Navigation";
+import { gql, request } from 'graphql-request';
+
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Radio,
+  RadioGroup,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from '@headlessui/react'
+import { StarIcon } from '@heroicons/react/20/solid'
+import { HeartIcon, MinusIcon, PlusIcon } from '@heroicons/react/24/outline'
+
+const product = {
+  name: 'Zip Tote Basket',
+  price: '$140',
+  rating: 4,
+  images: [
+    {
+      id: 1,
+      name: 'Angled view',
+      src: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-03-product-01.jpg',
+      alt: 'Angled front view with bag zipped and handles upright.',
+    },
+    {
+        id: 2,
+        name: 'Angled view',
+        src: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-03-product-01.jpg',
+        alt: 'Angled front view with bag zipped and handles upright.',
+    },
+    {
+        id: 3,
+        name: 'Angled view',
+        src: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-03-product-01.jpg',
+        alt: 'Angled front view with bag zipped and handles upright.',
+    },
+    {
+        id: 4,
+        name: 'Angled view',
+        src: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-03-product-01.jpg',
+        alt: 'Angled front view with bag zipped and handles upright.',
+    }
+  ],
+  colors: [
+    { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700' },
+    { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400' },
+    { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500' },
+  ],
+  description: `
+    <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
+  `,
+  details: [
+    {
+      name: 'Features',
+      items: [
+        'Multiple strap configurations',
+        'Spacious interior with top zip',
+        'Leather handle and tabs',
+        'Interior dividers',
+        'Stainless strap loops',
+        'Double stitched construction',
+        'Water-resistant',
+      ],
+    },
+    // More sections...
+  ],
+}
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default async function Home() {
+
+//   const [selectedColor, setSelectedColor] = useState(product.colors[0])
+
+  const endpoint = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+  
+  const productsCategoriesQuery = gql`
+    query {
+      products (first: 4, where: {orderby: {field: DATE, order: DESC}}) {
+        nodes {
+          slug
+          productid: databaseId
+          name
+          onSale
+          sku
+          shortDescription
+          type
+          featured
+          image {
+            altText
+            id
+            sourceUrl
+            srcSet
+          }
+          ... on SimpleProduct {
+            price
+            regularPrice
+            id
+          }
+          ... on VariableProduct {
+            price
+            id
+            regularPrice
+          }
+          ... on ExternalProduct {
+            price
+            id
+            externalUrl
+            regularPrice
+          }
+        }
+      }
+      productCategories (first: 5) {
+        nodes {
+          id
+          name
+          slug
+          image {
+            altText
+            id
+            sourceUrl
+            srcSet
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await request(endpoint, productsCategoriesQuery) as { products: { nodes: any[] }, productCategories: { nodes: any[] } };
+  const products = data.products.nodes;
+  const categories = data.productCategories.nodes;
+  console.log(products);
+  console.log(categories);
+
+  return (
+   <>
+      <Navigation /> 
+        <main>
+        <div className="bg-white">
+            
+
+            <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
+                
+                <nav aria-label="Breadcrumb">
+                    <ol role="list" className="flex items-center space-x-4">
+                        <li>
+                        <div className="flex items-center">
+                            <a href="#" className="mr-4 text-sm font-medium text-gray-900">Women</a>
+                            <svg viewBox="0 0 6 20" aria-hidden="true" className="h-5 w-auto text-gray-300">
+                            <path d="M4.878 4.34H3.551L.27 16.532h1.327l3.281-12.19z" fill="currentColor" />
+                            </svg>
+                        </div>
+                        </li>
+                        <li>
+                        <div className="flex items-center">
+                            <a href="#" className="mr-4 text-sm font-medium text-gray-900">Clothing</a>
+                            <svg viewBox="0 0 6 20" aria-hidden="true" className="h-5 w-auto text-gray-300">
+                            <path d="M4.878 4.34H3.551L.27 16.532h1.327l3.281-12.19z" fill="currentColor" />
+                            </svg>
+                        </div>
+                        </li>
+
+                        <li className="text-sm">
+                        <a href="#" aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">Basic Tee</a>
+                        </li>
+                    </ol>
+                </nav>
+
+
+                <div className="mt-4 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                {/* Image gallery */}
+                <TabGroup className="flex flex-col-reverse">
+                    {/* Image selector */}
+                    <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+                    <TabList className="grid grid-cols-4 gap-6">
+                        {product.images.map((image) => (
+                        <Tab
+                            key={image.id}
+                            className="group relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-indigo-500/50 focus:ring-offset-4"
+                        >
+                            <span className="sr-only">{image.name}</span>
+                            <span className="absolute inset-0 overflow-hidden rounded-md">
+                            <img alt="" src={image.src} className="size-full object-cover" />
+                            </span>
+                            <span
+                            aria-hidden="true"
+                            className="pointer-events-none absolute inset-0 rounded-md ring-2 ring-transparent ring-offset-2 group-data-[selected]:ring-indigo-500"
+                            />
+                        </Tab>
+                        ))}
+                    </TabList>
+                    </div>
+
+                    <TabPanels>
+                    {product.images.map((image) => (
+                        <TabPanel key={image.id}>
+                        <img alt={image.alt} src={image.src} className="aspect-square w-full object-cover sm:rounded-lg" />
+                        </TabPanel>
+                    ))}
+                    </TabPanels>
+                </TabGroup>
+
+                {/* Product info */}
+                <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">{product.name}</h1>
+
+                    <div className="mt-3">
+                    <h2 className="sr-only">Product information</h2>
+                    <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
+                    </div>
+
+                    {/* Reviews */}
+                    <div className="mt-3">
+                    <h3 className="sr-only">Reviews</h3>
+                    <div className="flex items-center">
+                        <div className="flex items-center">
+                        {[0, 1, 2, 3, 4].map((rating) => (
+                            <StarIcon
+                            key={rating}
+                            aria-hidden="true"
+                            className={classNames(
+                                product.rating > rating ? 'text-indigo-500' : 'text-gray-300',
+                                'size-5 shrink-0',
+                            )}
+                            />
+                        ))}
+                        </div>
+                        <p className="sr-only">{product.rating} out of 5 stars</p>
+                    </div>
+                    </div>
+
+                    <div className="mt-6">
+                    <h3 className="sr-only">Description</h3>
+
+                    <div
+                        dangerouslySetInnerHTML={{ __html: product.description }}
+                        className="space-y-6 text-base text-gray-700"
+                    />
+                    </div>
+
+                    <form className="mt-6">
+                    {/* Colors */}
+                    {/* <div>
+                        <h3 className="text-sm font-medium text-gray-600">Color</h3>
+
+                        <fieldset aria-label="Choose a color" className="mt-2">
+                        <RadioGroup value={selectedColor} onChange={setSelectedColor} className="flex items-center gap-x-3">
+                            {product.colors.map((color) => (
+                            <Radio
+                                key={color.name}
+                                value={color}
+                                aria-label={color.name}
+                                className={classNames(
+                                color.selectedColor,
+                                'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none data-[checked]:ring-2 data-[focus]:data-[checked]:ring data-[focus]:data-[checked]:ring-offset-1',
+                                )}
+                            >
+                                <span
+                                aria-hidden="true"
+                                className={classNames(color.bgColor, 'size-8 rounded-full border border-black/10')}
+                                />
+                            </Radio>
+                            ))}
+                        </RadioGroup>
+                        </fieldset>
+                    </div> */}
+
+                    <div className="mt-10 flex">
+                        <button
+                        type="submit"
+                        className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
+                        >
+                        Add to bag
+                        </button>
+
+                        {/* <button
+                        type="button"
+                        className="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"
+                        >
+                        <HeartIcon aria-hidden="true" className="size-6 shrink-0" />
+                        <span className="sr-only">Add to favorites</span>
+                        </button> */}
+                    </div>
+                    </form>
+
+                    {/* <section aria-labelledby="detailed-description" className="py-12">
+                        <h2 id="detailed-description" className="sr-only">
+                            Detailed description
+                        </h2>
+                    
+                        <div className="divide-y divide-gray-200 border-t">
+                            <div
+                                dangerouslySetInnerHTML={{ __html: product.description }}
+                                className="space-y-6 text-base text-gray-700 mt-6"
+                            />
+                        </div>
+                    </section> */}
+
+                    {/* <section aria-labelledby="details-heading" className="mt-12">
+                    <h2 id="details-heading" className="sr-only">
+                        Additional details
+                    </h2>
+
+                    <div className="divide-y divide-gray-200 border-t">
+                        {product.details.map((detail) => (
+                        <Disclosure key={detail.name} as="div">
+                            <h3>
+                            <DisclosureButton className="group relative flex w-full items-center justify-between py-6 text-left">
+                                <span className="text-sm font-medium text-gray-900 group-data-[open]:text-indigo-600">
+                                {detail.name}
+                                </span>
+                                <span className="ml-6 flex items-center">
+                                <PlusIcon
+                                    aria-hidden="true"
+                                    className="block size-6 text-gray-400 group-hover:text-gray-500 group-data-[open]:hidden"
+                                />
+                                <MinusIcon
+                                    aria-hidden="true"
+                                    className="hidden size-6 text-indigo-400 group-hover:text-indigo-500 group-data-[open]:block"
+                                />
+                                </span>
+                            </DisclosureButton>
+                            </h3>
+                            <DisclosurePanel className="pb-6">
+                            <ul role="list" className="list-disc space-y-1 pl-5 text-sm/6 text-gray-700 marker:text-gray-300">
+                                {detail.items.map((item) => (
+                                <li key={item} className="pl-2">
+                                    {item}
+                                </li>
+                                ))}
+                            </ul>
+                            </DisclosurePanel>
+                        </Disclosure>
+                        ))}
+                    </div>
+
+                    </section> */}
+
+                </div>
+                </div>
+            </div>
+            </div>
+        </main>
+      <Footer />
+   </>
+  );
+}
